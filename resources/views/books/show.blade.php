484b2928686a3b5425099a6a46fcfa15cfedebdf
joaogabrieI[ <joaonalves1@gmail.com>
2024-05-03 11:31:45 -0300

    correção de margem

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -7,7 +7,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <a href="{{route('dashboard')}}" class="btn btn-dark mt-3 ms-5">Voltar</a>
+                <a href="{{route('dashboard')}}" class="btn btn-dark mt-3 ms-4">Voltar</a>
                 <div class="p-6 text-gray-900">
                     <div class="container">
                         <div class="row">
